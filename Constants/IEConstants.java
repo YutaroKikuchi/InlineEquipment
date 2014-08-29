d3991d93dc530703dccc54b0a614f4c0ee95654a
yusan <s1000614@sendai-nct.jp>
2014-08-29 21:28:01 +0900

    Renew IEConstants Add static BAND Add static BEF Add static DIS Add static CLSEN Add static COL Add static ROT Add static SPD

--- Constants/IEConstants.java
+++ Constants/IEConstants.java
@@ -1,5 +1,22 @@
 package Constants;
 
-public class IEConstants {
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.RegulatedMotor;
 
+public class IEConstants {
+private IEConstants(){
+		
+	}
+	
+	public static RegulatedMotor BAND = Motor.A;	//ベルトコンベア用モータ
+	public static RegulatedMotor BEF = Motor.B;		//前処理装置用モータ
+	public static RegulatedMotor DIS = Motor.C;		//排出ユニット用モータ
+	public static EV3ColorSensor CLSEN = new EV3ColorSensor(SensorPort.S3);	//処理ユニット用カラーセンサ
+	public static SensorMode COL = CLSEN.getMode(2);						//処理ユニット用カラーセンサ
+	
+	public static int ROT = 30;		//ベルトコンベア用モータを動かす角度
+	public static int SPD = 200;	//ベルトコンベア用モータを動かす速度
 }
