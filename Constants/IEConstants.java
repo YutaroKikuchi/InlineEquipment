d7bb916d8210d5ad3013cf4cf09f61655f9788f5
yusan <s1000614@sendai-nct.jp>
2014-09-21 01:16:32 +0300

    Renew class IEConstants  Add constants ROT2 BLK_LEV  Renew constants BAND BEF DIS ROT ROT2 BEF_ROT BEF_SPD

--- Constants/IEConstants.java
+++ Constants/IEConstants.java
@@ -13,22 +13,23 @@ private IEConstants(){
 		
 	}
 	
-	public static RegulatedMotor BAND = Motor.A;	//ベルトコンベア用モータ
-	public static RegulatedMotor BEF = Motor.B;		//前処理装置用モータ
-	public static RegulatedMotor DIS = Motor.C;		//排出ユニット用モータ
+	public static RegulatedMotor BAND = Motor.C;	//ベルトコンベア用モータ
+	public static RegulatedMotor BEF = Motor.D;		//前処理装置用モータ
+	public static RegulatedMotor DIS = Motor.A;		//排出ユニット用モータ
 	
 	public static EV3ColorSensor CLSEN = new EV3ColorSensor(SensorPort.S3);	//処理ユニット用カラーセンサ
 	public static SensorMode COL = CLSEN.getMode(2);						//処理ユニット用カラーセンサ
 	public static EV3UltrasonicSensor SonicSensor = new EV3UltrasonicSensor(SensorPort.S4);	//導入エリア用超音波センサ
 	public static SensorMode SNC = SonicSensor.getMode(0);	//超音波センサモード
 	
-	public static int ROT = 30;		//ベルトコンベア用モータを動かす角度
+	public static int ROT = 10;		//ベルトコンベア用モータを動かす角度
+	public static int ROT2 = 90;
 	public static int SPD = 200;	//ベルトコンベア用モータを動かす速度
 	
 	public static float BSC = 0;	//超音波センサ用基準値
 	
-	public static int BEF_ROT = 30;	//前処理装置用モータを動かす角度
-	public static int BEF_SPD = 200;//前処理装置用モータを動かす速度
+	public static int BEF_ROT = -60;	//前処理装置用モータを動かす角度
+	public static int BEF_SPD = 150;//前処理装置用モータを動かす速度
 	
 	public static int DIS_ROT = 22;	//排出勝利用モータを動かす角度
 	public static int DIS_SPD = 200;//排出処理用モータを動かす速度
@@ -48,4 +49,6 @@ private IEConstants(){
 	public static String COMP = "処理完了";		//ログに示す内容
 	public static String UNCOMP = "処理未完了";
 	public static String LOG = "ID,色,処理状況\n";
+	
+	public static double[] BLK_LEV ={0.01,0.01,0.01};
 }
