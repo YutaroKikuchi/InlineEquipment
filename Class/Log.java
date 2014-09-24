8f53984af411536c697485646d244a9e7676cb9d
yusan <s1000614@sendai-nct.jp>
2014-09-24 09:50:12 +0300

    Renew class Log  Renew constructor

--- Class/Log.java
+++ Class/Log.java
@@ -1,5 +1,7 @@
 package Class;
 
+import Constants.IEConstants;
+
 
 /**
  * @author yusan
@@ -13,7 +15,7 @@ public class Log {
 	public Sample Treat_Sample;
 
 	public Log(){
-		colorname="•s–¾";
+		colorname=IEConstants.UDF;
 		Take_After=false;
 	}
 
