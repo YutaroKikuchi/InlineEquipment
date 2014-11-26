370274b29810e14b692914b81e4050d13ec5fa37
yusan <s1000614@sendai-nct.jp>
2014-11-26 19:20:03 +0200

    Renew Class Operation_Part  add member OffButton  add method StopThread SetButton

--- Class/Operation_Part.java
+++ Class/Operation_Part.java
@@ -1,6 +1,7 @@
 package Class;
 
 import lejos.hardware.Button;
+import lejos.hardware.Key;
 import Constants.IEConstants;
 
 
@@ -12,15 +13,39 @@ import Constants.IEConstants;
 public class Operation_Part {
 
 	
-	public Operation_Button StartButton = new Operation_Button(IEConstants.START);
-	public Operation_Button StopButton = new Operation_Button(IEConstants.STOP);
+	public Operation_Button StartButton;
+	public Operation_Button StopButton;
+	public Operation_Button OffButton;
 	
 	public Operation_Part(){
+		StartButton = new Operation_Button(IEConstants.START);
+		StopButton = new Operation_Button(IEConstants.STOP);
+		OffButton = new Operation_Button(IEConstants.OFF);
 
 	}
 
 	public void finalize() throws Throwable {
 
+	}
+	
+	public void StopThread(Key in){
+		if(in == IEConstants.START){
+			StartButton.setrunable(false);
+		}else if(in == IEConstants.STOP){
+			StopButton.setrunable(false);
+		}else if(in == IEConstants.OFF){
+			OffButton.setrunable(false);
+		}
+	}
+	
+	public void SetButton(Key in){
+		if(in == IEConstants.START){
+			StartButton = new Operation_Button(in);
+		}else if(in == IEConstants.STOP){
+			StopButton = new Operation_Button(in);
+		}else if(in == IEConstants.OFF){
+			OffButton = new Operation_Button(in);
+		}
 	}
 
 }
