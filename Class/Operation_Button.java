91374492bd226dc13ba79359b37478bfd28d3ea3
yusan <s1000614@sendai-nct.jp>
2014-11-26 19:22:51 +0200

    Renew Operation_Button  create innerclass ButtonException  create sysem thread

--- Class/Operation_Button.java
+++ Class/Operation_Button.java
@@ -8,33 +8,61 @@ import lejos.hardware.Key;
  * @version 1.0
  * @created 2014/08/29 21:16:57
  */
-public class Operation_Button {
+public class Operation_Button extends Thread{
 
 	public Operation_Part Operation_Part;
 
-	String SwitchMode;
-	Key button;
+	private Key button;
+	private boolean buttonflag;
+	private boolean runable;
 	
 	public Operation_Button(Key button){
 		this.button = button;
+		this.buttonflag = false;
+		this.runable = false;
 	}
 
 	public void finalize() throws Throwable {
 
 	}
+	
+	public void run(){
+		runable = true;
+		buttonflag = false;
+		
+		while(runable==true){
+			try {
+				Pushdown();
+			} catch (ButtonException e) {
+				buttonflag = true;
+				runable=false;
+				break;
+			}
+		}
+	}
 
-	public void Pushdown(){
+	public void Pushdown() throws ButtonException{
 		if(button.isDown()==true){
 			Pushdown_Event();
 		}
 	}
 
-	public void Pushdown_Event(){
-		try{
-			System.out.println(10/0);
-		}catch(ArithmeticException err){
-			throw err;
-		}
+	public void Pushdown_Event() throws ButtonException{
+		throw new ButtonException("Pressed Button");
 	}
-
+	
+	public boolean getbuttonflag(){
+		return buttonflag;
+	}
+	
+	public void setrunable(boolean in){
+		runable = in;
+	}
+	
+	public class ButtonException extends Exception {
+	    private static final long serialVersionUID = 1L;
+	    public ButtonException(String message) {
+	        super(message);
+	    }
+	}
 }
